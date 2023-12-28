c80d262168f923512aa5f9ae0905bf392a4eaa0c
Ritesh Patil <ritesh@onebharat.tech>
2023-12-28 12:22:21 +0530

    feat: creates devs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import AppDemo from "@/components/AppDemo";
+import BuiltForDevs from "@/components/BuiltForDevs";
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <main className="flex min-h-screen w-screen overflow-x-hidden flex-col items-center justify-between pt-24">
       <Hero />
       <AppDemo />
+      <BuiltForDevs />
     </main>
   );
 }
